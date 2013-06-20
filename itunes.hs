55c2ce3a445449d899a19f0ba6593cbfeb639bd5
Chris Barrett <chris.d.barrett@me.com>
2013-06-21 02:48:19 +1200

    Tidy up

--- itunes.hs
+++ itunes.hs
@@ -10,7 +10,7 @@
  DESCRIPTION:
   Commands for working with iTunes from the command-line.
 
- DEPENDENCIES:
+ CABAL DEPENDENCIES:
   ansi-wl-pprint
 
 -}
@@ -44,53 +44,84 @@ showUsage =
   , "  add [items...]   Add files or folders to the iTunes library"
   , "  help             Show usage" ]
 
+
 execute :: Args -> IO ()
-execute Invalid = putStrLn "Invalid usage." >> showUsage >> exitFailure
-execute (Unknown cmd) = putStrLn ("Unrecognised command: " ++ cmd) >> execute Help >> exitFailure
-execute Help = putStrLn description >> showUsage
-  where description = "itunes: Commands for working with iTunes from the command-line"
+
+execute Invalid =
+  putStrLn "Invalid usage." >> showUsage >> exitFailure
+
+execute (Unknown cmd) =
+  putStrLn ("Unrecognised command: " ++ cmd) >> execute Help >> exitFailure
+
+execute Help =
+  putStrLn description >> showUsage
+  where description = "itunes: Commands for working with iTunes"
 
 execute (Add args) = do
-  -- Bail if the iTunes folder does not exist.
   itunesExists <- itunesMedia >>= doesDirectoryExist
   unless itunesExists $ putStrLn "Cannot find iTunes Media folder" >> exitFailure
-  -- Extract file paths from args.
-  paths <- forM args $ \path -> catch (canonicalizePath path)
-                                     (\(_::IOException) -> return path)
-  -- Warn if any of the files to add do not exist.
-  notExists <- filterM (liftM not . fileOrDirectoryExists) paths
-  unless (null notExists) $ do
-    putDoc $ yellow (text "Warning: the following items do not exist:") <> linebreak
-    mapM_ (putStrLn . ("  * " ++)) notExists
-
-  files <- liftM (filter isMedia . concat) $
-           filterM fileOrDirectoryExists paths >>= mapM getFilesInTree
-
-  -- Add the files to iTunes. Abort if there are no items to add.
-  when (null files) $ putStrLn "No items added." >> exitFailure
-  mapM_ addToItunes files
-
-  -- Prompt user whether to delete originals.
-  let n = length files
-  putStrLn $ "Delete original " ++ pluralize "item" n ++ "? (y/n) [n] "
-  shouldDelete <- getYesOrNo False
-  when shouldDelete $ do
-    forM_ files $ \x -> do
-      removeFile x
-      putDoc $ red (text "  D ") <+> text x <> linebreak
-
-    putStrLn $ "Deleted " ++ show n ++ " " ++ pluralize "item" n ++"."
-
-  exitSuccess
+  paths <- pathsFromArgs
+  warnWhereNotExists paths
+  media <- mediaFromPaths paths
+  when (null media) $ putStrLn "No media found." >> exitFailure
+  mapM_ addToItunes media
+  promptDeleteOriginals media
 
   where
+    pathsFromArgs =
+      forM args $ \path ->
+      (canonicalizePath path) `catch` (\(_::IOException) -> return path)
 
-    fileOrDirectoryExists x = do
-      d <- doesDirectoryExist x
-      f <- doesFileExist x
-      return $ f || d
+    warnWhereNotExists paths = do
+      notExists <- filterM (liftM not . fileOrDirectoryExists) paths
+      unless (null notExists) $ do
+        putDoc $ dullyellow (text "Warning: the following items do not exist:") <> linebreak
+        mapM_ (\x -> putDoc $ dullyellow (text "  ? ") <+> text x <> linebreak) notExists
 
-    isMedia file = takeExtension file `elem` [".m4a", ".mov", ".mp4"]
+    mediaFromPaths paths =
+      liftM (filter isMedia . concat)
+            (filterM fileOrDirectoryExists paths >>= mapM getFilesInTree)
+
+    isMedia file =
+      takeExtension file `elem` [".m4a", ".mov", ".mp4"]
+
+    promptDeleteOriginals files = do
+      let n = length files
+      putStrLn $ "Delete original " ++ pluralize "item" n ++ "? (y/n) [n] "
+      shouldDelete <- getYesOrNo False
+      when shouldDelete $ do
+        forM_ files $ \x -> do
+          removeFile x
+          putDoc $ red (text "  D ") <+> text x <> linebreak
+
+        putStrLn $ "Deleted " ++ show n ++ " " ++ pluralize "item" n ++"."
+
+
+itunesMedia :: IO String
+itunesMedia = do
+  home <- getHomeDirectory
+  return $ home </> "Music" </> "iTunes" </> "iTunes Media"
+
+addToItunes :: FilePath -> IO ()
+addToItunes file = do
+  media <- itunesMedia
+  let dest = media </> "Automatically Add to iTunes.localized" </> takeFileName file
+  copyFile file dest
+  putDoc $ green (text "  A ") <+> text (takeFileName file) <> linebreak
+
+getFilesInTree :: FilePath -> IO [FilePath]
+getFilesInTree d | takeFileName d `elem` [".", ".."] = return []
+getFilesInTree d = do
+  isDir <- doesDirectoryExist d
+  if isDir
+    then liftM concat $ getDirectoryContents d >>= mapM (getFilesInTree . (</>) d)
+    else return [d]
+
+fileOrDirectoryExists :: FilePath -> IO Bool
+fileOrDirectoryExists x = do
+  d <- doesDirectoryExist x
+  f <- doesFileExist x
+  return $ f || d
 
 type Count = Int
 pluralize :: String -> Count -> String
@@ -106,24 +137,3 @@ getYesOrNo deflt = do
     'n'  -> return False
     '\n' -> return deflt
     _    -> getYesOrNo deflt
-
-itunesMedia :: IO String
-itunesMedia = do
-  home <- getHomeDirectory
-  return $ home </> "Music" </> "iTunes" </> "iTunes Media"
-
-addToItunes :: FilePath -> IO ()
-addToItunes file = do
-  media <- itunesMedia
-  let dest = media </> "Automatically Add to iTunes.localized" </> takeFileName file
-  copyFile file dest
-  putDoc $ green (text "  A ") <+> text (takeFileName file) <> linebreak
-
--- Find all the files in the directory tree under the given file path.
-getFilesInTree :: FilePath -> IO [FilePath]
-getFilesInTree d | takeFileName d `elem` [".", ".."] = return []
-getFilesInTree d = do
-  isDir <- doesDirectoryExist d
-  if isDir
-    then liftM concat $ getDirectoryContents d >>= mapM (getFilesInTree . (</>) d)
-    else return [d]
