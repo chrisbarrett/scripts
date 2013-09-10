40aaf3e26dfbf0aed59a8497b6d3e5cdfbd518aa
Chris Barrett <chris.d.barrett@me.com>
2013-09-10 23:28:04 +1200

    itunes.hs

--- itunes.hs
+++ itunes.hs
@@ -113,7 +113,7 @@ mediaFromPath :: FilePath -> IO [FilePath]
 mediaFromPath path = do
   exists <- fileOrDirectoryExists path
   if exists
-    then getFilesInTree path >>= mapM categoriseType >>= mapM selectMedia >>= return . concat
+    then liftM concat $ getFilesInTree path >>= mapM categoriseType >>= mapM selectMedia
     else return []
 
 --- Inspect the given file and determine whether it is an actionable type.
