9d27139ce3263956e2fd3891185d3e314390ea5b
Chris Barrett <chris.d.barrett@me.com>
2013-09-10 23:24:50 +1200

    itunes.hs

--- itunes.hs
+++ itunes.hs
@@ -89,7 +89,7 @@ execute (Add args)    = do
         putStrLn $ "Deleted " ++ show n ++ " " ++ pluralize n "item" ++ "."
 
 --- Concatenate a monadic filepath with pure filepaths.
-(/>) :: Functor m => m FilePath -> FilePath -> m FilePath
+(/>) :: IO FilePath -> FilePath -> m FilePath
 io /> p = (</>) <$> io <*> pure p
 infix 4 />
 
