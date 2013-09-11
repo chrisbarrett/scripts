42c60c302360486af4e43e535717974b275e3130
Chris Barrett <chris.d.barrett@me.com>
2013-09-12 00:20:00 +1200

    itunes2.hs

--- itunes2.hs
+++ itunes2.hs
@@ -157,7 +157,6 @@ mediaFromPath p = do
   zip <- isZipFile p
   return $ case (dir, zip) of
     (True, _) -> liftM concat $ getFilesInTree p >>= mapM mediaFromPath
-    (_, True) -> (p, ZipFile p)
     _         -> []
 
 
