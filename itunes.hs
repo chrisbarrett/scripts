7df146eb4f8dc45a636da67e9c2949d9b38e1ea8
Chris Barrett <chris.d.barrett@me.com>
2013-09-12 00:36:00 +1200

    Stage new itunes script that supports zip extraction

--- itunes.hs
+++ itunes.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections       #-}
+{-# LANGUAGE ViewPatterns        #-}
 {-
 
  itunes
@@ -29,24 +31,15 @@ import           System.Directory
 import           System.Environment           (getArgs)
 import           System.Exit                  (exitFailure)
 import           System.FilePath.Posix
-import           System.IO.Temp
 import           Text.PrettyPrint.ANSI.Leijen (dullyellow, green, linebreak,
                                                putDoc, red, text, (<+>), (<>))
 
---- Enumerates the possible parsed values of the program arguments.
+--------------------------------------------------------------------------------
+-- Main program
+
+-- | Enumerates the possible parsed values of the program arguments.
 data Args = Add [FilePath] | Help | Invalid | Unknown String
           deriving Show
-
---- Enumerates the different media form that will be added to iTunes.
-data MediaType = File FilePath
-               | Stream L8.ByteString Name (Maybe ArchivePath)
-type Name = String
-type ArchivePath = FilePath
-
---- Enumerates different filetypes that need to be handled when searching for media.
-data FileType = Media MediaType
-              | Zip ArchivePath
-              | Unsupported
 
 main :: IO ()
 main = getArgs >>= execute . parseArgs
@@ -56,7 +49,7 @@ main = getArgs >>= execute . parseArgs
     parseArgs (x:_) = Unknown x
     parseArgs _     = Invalid
 
---- Print program usage to stdout.
+-- | Print program usage to stdout.
 showUsage :: IO ()
 showUsage =
   putStrLn $ unlines
@@ -64,26 +57,30 @@ showUsage =
   , "  add [items...]   Add files or folders to the iTunes library"
   , "  help             Show usage" ]
 
---- Run the program as specified by the program arguments.
+-- | Run the program as specified by the program arguments.
 execute :: Args -> IO ()
 execute Help          = putStrLn "itunes: Commands for working with iTunes" >> showUsage
 execute Invalid       = putStrLn "Invalid usage." >> showUsage >> exitFailure
 execute (Unknown cmd) = putStrLn ("Unrecognised command: " ++ cmd) >> showUsage >> exitFailure
 execute (Add args)    = do
-  itunesExists <- itunesMedia >>= doesDirectoryExist
+  itunesExists <- itunesImportFolder >>= doesDirectoryExist
   unless itunesExists $ putStrLn "Cannot find iTunes Media folder" >> exitFailure
   paths <- pathsFromArgs
   warnWhereNotExists paths
-  media <- liftM concat $ mapM mediaFromPath paths
-  when (null media) $ putStrLn "No media found." >> exitFailure
-  mapM_ addToItunes media
-  promptDeleteOriginals media
+  xs <- liftM concat $ mapM mediaFromPath paths
+  when (null xs) $ putStrLn "No media found." >> exitFailure
+
+  let files = map fst xs
+      media = map snd xs
+  mapM_ importMedia media
+  promptDeleteOriginals files
 
   where
-    pathsFromArgs =
-      forM args $ \path ->
+    -- | Extract targets to be imported from program arguments.
+    pathsFromArgs = forM args $ \path ->
         canonicalizePath path `catch` (\(_::IOException) -> return path)
 
+    -- | Warn when trying to import items that do not exist on the filesystem.
     warnWhereNotExists paths = do
       notExists <- filterM (liftM not . fileOrDirectoryExists) paths
       unless (null notExists) $ do
@@ -94,52 +91,76 @@ execute (Add args)    = do
                     <> linebreak)
           notExists
 
-    promptDeleteOriginals :: [MediaType] -> IO ()
-    promptDeleteOriginals files = do
-      let n = length files
+    -- | Prompt the user whether to delete the original items after importing.
+    promptDeleteOriginals :: [FilePath] -> IO ()
+    promptDeleteOriginals xs = do
+      let n = length xs
       putStrLn $ "Delete original " ++ pluralize n "item" ++ "? (y/n) [n] "
       shouldDelete <- getYesOrNo False
       when shouldDelete $ do
-        forM_ files deleteMedia
+        forM_ xs $ \x -> do
+          removeFile x
+          putDoc $ red (text "  D ") <+> text x <> linebreak
+
         putStrLn $ "Deleted " ++ show n ++ " " ++ pluralize n "item" ++ "."
 
-    deleteMedia :: MediaType -> IO ()
-    deleteMedia (File path) = do
-      removeFile path
-      putDoc $ red (text "  D ") <+> text path <> linebreak
-    deleteMedia (Stream _ _ (Just archive)) = undefined
+    -- | The path to the iTunes import folder.
+    itunesImportFolder :: IO FilePath
+    itunesImportFolder  = ( getHomeDirectory /> "Music" </> "iTunes" </> "iTunes Media" )
+                          /> "Automatically Add to iTunes.localized"
 
---- Concatenate a monadic filepath with pure filepaths.
+    -- | Import each media item into iTunes.
+    importMedia :: Importable -> IO ()
+    importMedia x = do
+      dest <- itunesImportFolder
+      tasks <- importTasks dest x
+      forM_ tasks $ \t -> do
+        runTask t
+        putDoc $ green (text "  A ") <+> text (taskName t)  <> linebreak
+
+
+-- | Concatenate a monadic filepath with pure filepaths.
 (/>) :: IO FilePath -> FilePath -> IO FilePath
 io /> p = (</>) <$> io <*> pure p
 infix 4 />
 
---- The path to the iTunes library in the user's home folder.
-itunesMedia :: IO FilePath
-itunesMedia = getHomeDirectory /> "Music" </> "iTunes" </> "iTunes Media"
+-- | Test whether the given file or directory exists.
+fileOrDirectoryExists :: FilePath -> IO Bool
+fileOrDirectoryExists x = or <$> sequence [doesDirectoryExist x, doesFileExist x]
 
---- Add the given media to the iTunes library.
-addToItunes :: MediaType -> IO ()
-addToItunes (File file) = do
-  dest <- itunesMedia /> "Automatically Add to iTunes.localized" </> takeFileName file
-  copyFile file dest
-  putDoc $ green (text "  A ") <+> text (takeFileName file) <> linebreak
-addToItunes (Stream bs name _) = undefined
+type Count = Int
+-- | Perform a naive string pluralisation.
+pluralize :: Count -> String -> String
+pluralize 1 str = str
+pluralize _ str = str ++ "s"
 
-isMedia :: FilePath -> Bool
-isMedia p = p `elem` [".m4a", ".m4v", ".mov", ".mp4", ".mp3", ".mpg", ".aac", ".aiff"]
+type Default = Bool
+-- | Prompt the user for a yes or no response, with a default answer.
+getYesOrNo :: Default -> IO Bool
+getYesOrNo deflt = do
+  ch <- getChar
+  case toLower ch of
+    'y'  -> return True
+    'n'  -> return False
+    '\n' -> return deflt
+    _    -> getYesOrNo deflt
+
+--------------------------------------------------------------------------------
+-- Filesystem utilities
+
+-- | Filter the input files for importable items.
+mediaFromPath :: FilePath -> IO [(FilePath, Importable)]
+mediaFromPath p@(isMedia -> True) = return [ (p, MediaFile p) ]
+mediaFromPath p = do
+  isDir <- doesDirectoryExist p
+  isZip <- isZipFile p
+  case (isDir, isZip) of
+    (True, _) -> liftM concat $ getFilesInTree p >>= mapM mediaFromPath
+    (_, True) -> return [ (p, ZipFile p)  ]
+    _         -> return []
 
 
-
-asZipFile :: FilePath -> IO Bool
-asZipFile p = do
-  -- Read magic string from file header to determine type.
-  bs <- liftM (L8.unpack . L8.take 2) (L8.readFile p)
-  return $ case bs of
-    "PK" -> Just $ Zip p
-    _    -> Nothing
-
---- Walk the directory tree to find all files below a given path.
+-- | Walk the directory tree to find all files below a given path.
 getFilesInTree :: FilePath -> IO [FilePath]
 getFilesInTree d | takeFileName d `elem` [".", ".."] = return []
 getFilesInTree d = do
@@ -150,23 +171,39 @@ getFilesInTree d = do
     (_, True) -> return [d]
     _         -> return []
 
---- Test whether the given file or directory exists.
-fileOrDirectoryExists :: FilePath -> IO Bool
-fileOrDirectoryExists x = or <$> sequence [doesDirectoryExist x, doesFileExist x]
+-- | Create tasks to add the given media to the iTunes library.
+importTasks :: FilePath -> Importable -> IO [ImportTask]
 
-type Count = Int
---- Perform a naive string pluralisation.
-pluralize :: Count -> String -> String
-pluralize 1 str = str
-pluralize _ str = str ++ "s"
+importTasks dest (MediaFile f) =
+  return [ ImportTask { taskName = takeFileName f
+                      , runTask = copyFile f $ dest </> takeFileName f } ]
 
-type Default = Bool
---- Prompt the user for a yes or no response, with a default answer.
-getYesOrNo :: Default -> IO Bool
-getYesOrNo deflt = do
-  ch <- getChar
-  case toLower ch of
-    'y'  -> return True
-    'n'  -> return False
-    '\n' -> return deflt
-    _    -> getYesOrNo deflt
+importTasks dest (ZipFile f) = withArchive f $ do
+  entries <- liftM (filter isMedia) entryNames
+  forM entries $ \x ->
+    return ImportTask { taskName = x
+                      , runTask = withArchive f $ extractFiles [x] dest
+                      }
+
+--------------------------------------------------------------------------------
+-- Common types
+
+-- | Associates an item to import with a label for UI feedback.
+data ImportTask = ImportTask
+                  { taskName :: String
+                  , runTask  :: IO () }
+
+data Importable = MediaFile FilePath
+                | ZipFile FilePath
+
+--------------------------------------------------------------------------------
+
+-- | True if the given file can be imported by iTunes.
+isMedia :: FilePath -> Bool
+isMedia p = takeExtension p `elem` [".m4a", ".m4v", ".mov", ".mp4", ".mp3", ".mpg", ".aac", ".aiff"]
+
+-- | Read file header to test whether the given path points to a zip archive.
+isZipFile :: FilePath -> IO Bool
+isZipFile p = do
+  header <- liftM (L8.unpack . L8.take 2) (L8.readFile p)
+  return $ header == "PK"
