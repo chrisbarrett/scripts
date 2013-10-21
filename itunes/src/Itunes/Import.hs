ce7b26d628e7149415a773f5c264141e9f96baa2
Chris Barrett <chris.d.barrett@me.com>
2013-10-21 20:59:05 +1300

    itunes/src/Itunes/Import.hs

--- itunes/src/Itunes/Import.hs
+++ itunes/src/Itunes/Import.hs
@@ -3,13 +3,12 @@ module Import
          addToItunes
        )
        where
-import           System.Exit                  (exitFailure)
-
 import           Control.Applicative
 import           Control.Monad
 import           Data.Char                    (toLower)
 import           Media
 import           System.Directory
+import           System.Exit                  (exitFailure)
 import           Text.PrettyPrint.ANSI.Leijen (dullyellow, green, linebreak,
                                                putDoc, red, text, (<+>), (<>))
 addToItunes :: [FilePath] -> IO ()
