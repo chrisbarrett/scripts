a4344617e505b2f19ec7603489b7f7c09bcf5d40
Chris Barrett <chris.d.barrett@me.com>
2013-10-21 20:45:03 +1300

    itunes/src/Main.hs

--- itunes/src/Main.hs
+++ itunes/src/Main.hs
@@ -1,16 +1,11 @@
 module Main where
 
-import           Codec.Archive.Zip
-import           Control.Applicative
 import           Control.Exception
-import           Control.Monad
-import qualified Data.ByteString.Lazy.Char8   as L8
 import           Data.Char                    (toLower)
 import           Prelude                      hiding (catch)
 import           System.Directory
 import           System.Environment           (getArgs)
 import           System.Exit                  (exitFailure)
-import           System.FilePath.Posix
 import           Text.PrettyPrint.ANSI.Leijen (dullyellow, green, linebreak,
                                                putDoc, red, text, (<+>), (<>))
 
@@ -124,74 +119,3 @@ getYesOrNo deflt = do
     'n'  -> return False
     '\n' -> return deflt
     _    -> getYesOrNo deflt
-
---------------------------------------------------------------------------------
--- Common types
-
--- | Associates an item to import with a label for UI feedback.
-data ImportTask = ImportTask
-                  { taskName :: String
-                  , runTask  :: IO () }
-
-data Importable = MediaFile FilePath
-                | ZipFile FilePath
-
---------------------------------------------------------------------------------
--- Media parsing
-
--- | Filter the input files for importable items.
-mediaFromPath :: FilePath -> IO [(FilePath, Importable)]
-mediaFromPath p = do
-  isDir <- doesDirectoryExist p
-  isMedia <- isMediaFile p
-  isZip <- isZipFile p
-  case (isDir, isMedia, isZip) of
-    (True, _, _) -> liftM concat $ getFilesInTree p >>= mapM mediaFromPath
-    (_, True, _) -> return [ (p, MediaFile p) ]
-    (_, _, True) -> return [ (p, ZipFile p) ]
-    _            -> return []
-
-
--- | Walk the directory tree to find all files below a given path.
-getFilesInTree :: FilePath -> IO [FilePath]
-getFilesInTree d | takeFileName d `elem` [".", ".."] = return []
-getFilesInTree d = do
-  isDir <- doesDirectoryExist d
-  isFile <- doesFileExist d
-  case (isDir, isFile) of
-    (True, _) -> concat <$> (getDirectoryContents d >>= mapM (getFilesInTree . (</>) d))
-    (_, True) -> return [d]
-    _         -> return []
-
-
--- | Create tasks to add the given media to the iTunes library.
-importTasks :: FilePath -> Importable -> IO [ImportTask]
-
-importTasks dest (MediaFile f) =
-  return [ ImportTask { taskName = takeFileName f
-                      , runTask = copyFile f $ dest </> takeFileName f } ]
-
-importTasks dest (ZipFile f) = withArchive f $ do
-  entries <- liftM (filter hasMediaExt) entryNames
-  forM entries $ \x ->
-    return ImportTask { taskName = x
-                      , runTask = withArchive f $ extractFiles [x] dest }
-
--- | True if the given file can be imported by iTunes.
-isMediaFile :: FilePath -> IO Bool
-isMediaFile p = do
-  exists <- doesFileExist p
-  return $ exists && hasMediaExt p
-
--- | True if the given file has a media file extension.
-hasMediaExt :: FilePath -> Bool
-hasMediaExt p = takeExtension p `elem` [".m4a", ".m4v", ".mov", ".mp4", ".mp3", ".mpg", ".aac", ".aiff"]
-
--- | Read file header to test whether the given path points to a zip archive.
-isZipFile :: FilePath -> IO Bool
-isZipFile p = do
-  isFile <- doesFileExist p
-  if isFile
-    then do header <- liftM (L8.unpack . L8.take 2) (L8.readFile p)
-            return $ header == "PK"
-    else return False
