02fa70900c248ca7c0cd53ddb589641c12dd6355
Chris Barrett <chris.d.barrett@me.com>
2013-10-25 10:19:48 +1300

    reinstall-macports.sh

--- reinstall-macports.sh
+++ reinstall-macports.sh
@@ -6,6 +6,11 @@
 #
 # Source: <https://trac.macports.org/wiki/Migration>
 
+if [ y_or_n "Reinstall Macports and all ports. Continue?" ]; then
+    echo aborted.
+    exit 1
+fi
+
 cd
 # save list of ports
 port -qv installed > ports.txt
