161dcdf234c0b8c288b2e0c72b56a02fd27f6140
Chris Barrett <chris.d.barrett@me.com>
2013-10-25 10:25:07 +1300

    reinstall-macports.sh

--- reinstall-macports.sh
+++ reinstall-macports.sh
@@ -20,4 +20,4 @@ port clean all
 # Restore scripts.
 curl -O https://svn.macports.org/repository/macports/contrib/restore_ports/restore_ports.tcl
 chmod +x restore_ports.tcl
-sudo ./restore_ports.tcl ports.txt
+./restore_ports.tcl ports.txt
