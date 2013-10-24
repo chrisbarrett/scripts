0426a9cd5aa63f70d2fb09f2590d443f37c486f3
Chris Barrett <chris.d.barrett@me.com>
2013-10-25 10:25:03 +1300

    reinstall-macports.sh

--- reinstall-macports.sh
+++ reinstall-macports.sh
@@ -11,5 +11,13 @@ if [ y_or_n "WARNING: MacPorts and all ports will be reinstalled. Continue?" ]; 
     exit 1
 fi
 cd
-# save list of ports
+# Save list of ports.
 port -qv installed > ports.txt
+# Uninstall all ports.
+port -f uninstall installed
+# Clean any partially-completed builds.
+port clean all
+# Restore scripts.
+curl -O https://svn.macports.org/repository/macports/contrib/restore_ports/restore_ports.tcl
+chmod +x restore_ports.tcl
+sudo ./restore_ports.tcl ports.txt
